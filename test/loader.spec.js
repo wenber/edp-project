9e818bac5e3b315ca5d588bfad717de113f2d0a1
leeight <leeight@gmail.com>
2014-04-28 17:14:27 +0800

    Support preserveBaseUrl file metadata

--- test/loader.spec.js
+++ test/loader.spec.js
@@ -14,10 +14,12 @@
  * @description 
  * lib/loader.js的测试用例
  **/
+var fs = require( 'fs' );
 var path = require( 'path' );
 
 var Project = path.resolve(__dirname, 'data', 'dummy-project');
 var Loader = require( '../lib/loader' );
+var ReadLoaderConfig = require( '../lib/util/read-loader-config' );
 
 describe('loader', function() {
     it('getConfig default', function(){
@@ -78,6 +80,45 @@ describe('loader', function() {
         expect( oldPackages[0].name ).toBe( 'hello' );
         expect( oldPackages[0].location ).toBe( '../dep/hello/0.0.2/src' );
         expect( oldPackages[0].main ).toBe( 'main' );
+    });
+
+    it( 'updateAllFilesConfig', function(){
+        var Project = path.resolve( __dirname, 'data', 'loader', '1' );
+        var projectInfo = {
+            infoDir: path.resolve( Project, '.edpproj' ),
+            dir: Project
+        };
+        var Map = {};
+        spyOn( fs, 'writeFileSync' ).andCallFake( function( file, data, encoding ){
+          Map[ path.relative( __dirname, file ) ] = data;
+        });
+        Loader.updateAllFilesConfig( projectInfo );
+
+        expect( Map[ 'data/loader/1/src/1.html' ] ).not.toBeUndefined();
+        expect( Map[ 'data/loader/1/src/2.html' ] ).not.toBeUndefined();
+
+        function getConfig( f ) {
+            var c = Map[ f ] || fs.readFileSync( f, 'utf-8' );
+            return ReadLoaderConfig( c, f );
+        }
+
+        var c1 = getConfig( 'data/loader/1/src/1.html' );
+        var c2 = getConfig( 'data/loader/1/src/2.html' );
+        expect( c1 ).not.toBeUndefined();
+        expect( c1.data ).not.toBeUndefined();
+        expect( c2 ).not.toBeUndefined();
+        expect( c2.data ).not.toBeUndefined();
+        expect( c1.data.baseUrl ).toBe( '../src' );
+        expect( c2.data.baseUrl ).toBe( 'http://我/是/不/会/变/的' );
+        expect( c1.data.packages.length ).toBe( 5 );
+        expect( c2.data.packages.length ).toBe( 5 );
+
+        // loaderAutoConfig 设置为false，不会修改当前文件的require.config的内容
+        var c3 = getConfig( 'data/loader/1/src/3.html' );
+        expect( c3 ).not.toBeUndefined();
+        expect( c3.data ).not.toBeUndefined();
+        expect( c3.data.baseUrl ).toBe( 'http://我/是/不/会/变/的' );
+        expect( c3.data.packages.length ).toBe( 0 );
     });
 });
 
